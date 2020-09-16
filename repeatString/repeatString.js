a8d92c3cb2e9e21f454f302b64f9e287cfb6ca5f
d92563 <dinel_bob@yahoo.com>
2020-09-15 17:01:55 -0700

    repeatString function complete

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+if (n === 0) {
+    return "";
+} else if (n < 0) {
+    return "ERROR";
+  } else {
+    let result = "";
+    for (let i = 1; i <= n; i++) {
+       result += str;
+    } 
+    return result;
+}
+ 
 }
 
 module.exports = repeatString
