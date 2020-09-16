2187a00d58b46748dc36d85e2db861fd6c687121
d92563 <dinel_bob@yahoo.com>
2020-09-16 16:20:16 -0700

    sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+
+    let sum = 0;
+    if (a < 0 || b < 0 || typeof a !== "number" || typeof b !== "number") {
+        return "ERROR";
+    } 
+    if (a > b) {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum
 
 }
 
