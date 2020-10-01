8bca7ef4900f0535fca33820d279a70557aa9914
d92563 <dinel_bob@yahoo.com>
2020-09-30 17:14:36 -0700

    DOM exercises updated

--- helloWorld/helloWorld.js
+++ helloWorld/helloWorld.js
@@ -1,12 +1,12 @@
 const p = document.createElement('p');
 p.setAttribute('style', 'color: red')
 p.textContent = "Hey I'm red!";
-const body = document.querySelector('body');
-body.append(p);
+const container = document.querySelector('#container');
+container.append(p);
 const h3 = document.createElement('h3');
 h3.setAttribute("style", "color: blue;");
 h3.textContent = "I'm blue h3!";
-body.append(h3);
+container.append(h3);
 const div = document.createElement('div');
 div.setAttribute("style", "background-color: pink; border: 2px solid black;");
 const h1 = document.createElement('h1');
@@ -15,4 +15,4 @@ div.append(h1);
 const p1 = document.createElement('p');
 p1.textContent = "ME TOO!"
 div.append(p1);
-body.append(div);
+container.append(div);
