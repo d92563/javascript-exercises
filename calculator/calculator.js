6ee2e4694054b99e033c46ceffb885095f17d100
d92563 <dinel_bob@yahoo.com>
2020-10-27 16:57:55 -0700

    finished

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,47 @@
-function add () {
+function add (a, b) {
+	let result = a + b;
+	return result;
+}
+
+function subtract (a, b) {
+	let result = a - b;
+	return result;
+}
+
+function sum (arr) {
+	let result = 0;
+	if(arr.length !== 0) {
+	return result =	arr.reduce((acc, curr) => acc += curr, 0);
+	}
+	return result;
 	
 }
 
-function subtract () {
+function multiply (arr) {
 	
+	if(arr.length !== 0) {
+	let result = arr.reduce((acc, curr) => acc *= curr, 1);
+	return result;
+	} else {
+		return 0;
+	}
 }
 
-function sum () {
-	
+function power(a, b) {
+	let result = 1;
+	for (let i = 1; i <= b; i++) {
+		result *= a;
+	}
+  return result;
 }
 
-function multiply () {
-	
-}
-
-function power() {
-	
-}
-
-function factorial() {
-	
+function factorial(n) { 
+	let result
+	if (n <= 1) {
+		return 1;
+	} else {
+	return n * factorial(n-1);
+    }
 }
 
 module.exports = {
